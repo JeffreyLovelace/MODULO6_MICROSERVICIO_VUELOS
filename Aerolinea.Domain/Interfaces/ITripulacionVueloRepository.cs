02e462d895245c22ab70b59e14b4c8e7fe39cc72
jlovelace <lovelace@innovapplications.com>
2022-10-19 20:30:09 -0400

    commit

---
+++ Aerolinea.Domain/Interfaces/ITripulacionVueloRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Aerolinea.Vuelos.Domain.Entities;
+using Sharedkernel.Core;
+
+namespace Aerolinea.Vuelos.Domain.Interfaces {
+    public interface ITripulacionVueloRepository : InterfaceGeneric<TripulacionVuelo, Guid> {
+        Task UpdateAsync(TripulacionVuelo obj);
+        Task<ICollection<TripulacionVuelo>> ListTripulantes(String codGrupoTripulante);
+    }
+}
---
+++ Aerolinea.Infrastructure/Repositories/TripulacionVueloRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Aerolinea.Vuelos.Domain.Entities;
+using Aerolinea.Vuelos.Domain.Interfaces;
+using Aerolinea.Vuelos.Infrastructure.EF.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aerolinea.Vuelos.Infrastructure.Repositories {
+    public class TripulacionVueloRepository : ITripulacionVueloRepository {
+
+        public readonly DbSet<TripulacionVuelo> _tripulacion;
+
+        public TripulacionVueloRepository(WriteDbContext context) {
+            _tripulacion = context.tripulacionVuelo;
+        }
+        public async Task CreateAsync(TripulacionVuelo obj) {
+            await _tripulacion.AddAsync(obj);
+        }
+
+        public Task<TripulacionVuelo> FindByIdAsync(Guid id) {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ICollection<TripulacionVuelo>> ListTripulantes(String codGrupoTripulante) {
+            return (ICollection<TripulacionVuelo>)await _tripulacion
+               .SingleOrDefaultAsync(x => x.codGrupo == codGrupoTripulante && x.activo == 0);
+        }
+
+        public Task UpdateAsync(TripulacionVuelo obj) {
+            _tripulacion.Update(obj);
+            return Task.CompletedTask;
+        }
+    }
+}
